4f046ca49af42fb586534d9d1b4f6f9e33ee0fa4
brobro332 <332bro@naver.com>
2025-06-16 21:53:08 +0900

    기능추가 : 공통 응답 및 글로벌 인터셉터 개발

---
+++ src/common/interceptors/common-response.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { CommonResponseDto } from '../dtos/common-response.dto';
+
+@Injectable()
+export class CommonResponseInterceptor<T>
+  implements NestInterceptor<T, CommonResponseDto<T>>
+{
+  intercept(
+    _context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<CommonResponseDto<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        return new CommonResponseDto<T>(true, 'success', data);
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CommonResponseInterceptor } from './common/interceptors/common-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new CommonResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
