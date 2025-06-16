42be5741797e274e8f6a35d092e09add872f8fb8
김진형 <332bro@naver.com>
2025-06-17 00:03:06 +0900

    프로젝트 : Docker Compose 환경 구성 및 PostgreSQL 연동 설정 (#4)
    1. Docker Compose 환경 구성
    2. env 파일 환경설정 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: +configService.get('POSTGRES_EXTERNAL_PORT'),
+        username: configService.get('DATA_POSTGRES_USERNAME'),
+        password: configService.get('DATA_POSTGRES_PASSWORD'),
+        database: configService.get('DATA_POSTGRES_DB'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
